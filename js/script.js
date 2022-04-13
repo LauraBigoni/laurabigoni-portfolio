816e16ebd76d39b7558255557df73aaf65f99e76
Laura Bigoni <laurabigoni.92@gmail.com>
2022-04-13 17:20:25 +0200

    added js event listeners

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const show = document.querySelectorAll('.fade-in');
+
+const classes = {
+  home: show.item(0),
+  about: show.item(1),
+  work: show.item(2),
+  contacts: show.item(3),
+}
+
+const slider = {
+  home: document.querySelector('.home'),
+  about: document.querySelector('.about'),
+  work: document.querySelector('.work'),
+  contacts: document.querySelector('.contacts'),
+}
+
+function animateHome() {
+  slider.home.addEventListener('click', function () {
+    classes.home.classList.add('show');
+    classes.work.classList.remove('show');
+    classes.about.classList.remove('show');
+    classes.contacts.classList.remove('show');
+  });
+}
+
+function animateAbout() {
+  slider.about.addEventListener('click', function () {
+    classes.about.classList.add('show');
+    classes.work.classList.remove('show');
+    classes.home.classList.remove('show');
+    classes.contacts.classList.remove('show');
+  });
+}
+
+function animateWork() {
+  slider.work.addEventListener('click', function () {
+    classes.work.classList.add('show');
+    classes.about.classList.remove('show');
+    classes.home.classList.remove('show');
+    classes.contacts.classList.remove('show');
+  });
+}
+
+function animateContacts() {
+  slider.contacts.addEventListener('click', function () {
+    classes.contacts.classList.add('show');
+    classes.work.classList.remove('show');
+    classes.home.classList.remove('show');
+    classes.about.classList.remove('show');
+  });
+}
+
+animateHome();
+animateAbout();
+animateWork();
+animateContacts();
